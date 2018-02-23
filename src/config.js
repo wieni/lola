f036e35202e34b03f0be800e1e40fa3bfedca21d
Bart Van Thillo <bart@wieni.be>
2018-02-23 11:51:32 +0100

    re #3

--- src/config.js
+++ src/config.js
@@ -25,10 +25,13 @@ class Config {
 
     static async validateConfig(config) {
         const newConfig = config;
+
+        // Projects have a name.
         if (!newConfig.project) {
             throw new Error('"project" not found in config file');
         }
 
+        // Projects have a region.
         if (!newConfig.region) {
             newConfig.region = await inquirer.prompt([
                 {
@@ -40,12 +43,15 @@ class Config {
             ]);
         }
 
+        // Projects have stacks.
         if (!newConfig.stacks) {
             throw new Error('"stacks" not found in config file');
         } else if (Object.keys(newConfig.stacks).length === 0) {
             throw new Error('No stacks defined in config file');
         }
 
+        // Projects have a profile to access AWS
+        // TODO Or config file? Or keys?.
         if (!newConfig.profile) {
             newConfig.profile = await inquirer.prompt([
                 {
@@ -56,6 +62,14 @@ class Config {
             ]);
         }
 
+        // Projects have environments.
+        if (!newConfig.environments) {
+            throw new Error('"environments" not found in config file. Please provide a "default" one at least.');
+        } else if (Object.keys(newConfig.environments).length === 0) {
+            throw new Error('No environments defined in config file. Please provide a "default" one at least.');
+        }
+
+        // Stacks might have actions.
         await Promise.all(Object.keys(newConfig.stacks).map(async (stack) => {
             if (newConfig.stacks[stack].actions) {
                 await Promise.all(Object.keys(newConfig.stacks[stack].actions).map(async (action) => {
